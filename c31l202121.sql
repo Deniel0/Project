ab49ed37e48b942d7754137d9a78f4ccac98cd61
Deniel0 <sutod820@gmail.com>
2022-01-20 12:09:21 +0100

    GPU_table

--- c31l202121.sql
+++ c31l202121.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 15, 2022 at 05:36 PM
+-- Generation Time: Jan 20, 2022 at 12:09 PM
 -- Server version: 10.3.29-MariaDB-0+deb10u1
 -- PHP Version: 7.4.23
 
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `case_pc` (
   `case_id` int(11) NOT NULL,
   `Manufacturer_id` int(11) NOT NULL,
-  `length_id` int(11) NOT NULL,
   `ff_id` int(11) NOT NULL,
-  `Name` text NOT NULL
+  `Name` text NOT NULL,
+  `Max_Length` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -263,27 +263,99 @@ INSERT INTO `form factor` (`ff_id`, `Form Factor`) VALUES
 CREATE TABLE `gpu` (
   `gpu_id` int(11) NOT NULL,
   `Manufacturer_id` int(11) NOT NULL,
-  `length_id` int(11) NOT NULL,
-  `Video chipset` text NOT NULL,
+  `Type` text NOT NULL,
+  `Name` text NOT NULL,
   `Memory Size` text NOT NULL,
   `Memory Type` text NOT NULL,
-  `Memory Bandwidth` text NOT NULL,
-  `Width` text NOT NULL,
-  `Length` text NOT NULL,
-  `Thickness` text NOT NULL
+  `Core Clock` text NOT NULL,
+  `Boost Clock` text NOT NULL,
+  `Length` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `gpu length`
+-- Dumping data for table `gpu`
 --
 
-CREATE TABLE `gpu length` (
-  `length_id` int(11) NOT NULL,
-  `Length` int(11) NOT NULL,
-  `Max GPU Length` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `gpu` (`gpu_id`, `Manufacturer_id`, `Type`, `Name`, `Memory Size`, `Memory Type`, `Core Clock`, `Boost Clock`, `Length`) VALUES
+(1, 2, 'GeForce GTX', '1050', '2GB', 'GDDR5', '1442 MHz', '1569 MHz', 241),
+(2, 2, 'GeForce GTX', '1050Ti', '4GB', 'GDDR5', '1379 MHz', '1506 MHz', 241),
+(3, 2, 'GeForce GTX', '1060', '3GB', 'GDDR5', '1594 MHz', '1809 MHz', 242),
+(4, 2, 'GeForce GTX', '1060', '6GB', 'GDDR5', '1620 MHz', '1873 MHz', 298),
+(5, 2, 'GeForce GTX', '1070', '8GB', 'GDDR5', '1506 MHz', '1721 MHz', 298),
+(6, 2, 'GeForce GTX', '1070Ti', '8GB', 'GDDR5', '1607 MHz', '1759 MHz', 298),
+(7, 2, 'GeForce GTX', '1080', '8GB', 'GDDR5', '1759 MHz', '1936 MHz', 298),
+(8, 2, 'GeForce GTX', '1080Ti', '11GB', 'GDDR5X', '1569 MHz', '1708 MHz', 298),
+(9, 9, 'GeForce GTX', '1050', '2GB', 'GDDR5', '1392 MHz', '1531 MHz', 229),
+(10, 9, 'GeForce GTX', '1050Ti', '4GB', 'GDDR5', '1354 MHz', '1468 MHz', 229),
+(11, 9, 'GeForce GTX', '1060', '3GB', 'GDDR5', '1556 MHz', '1797 MHz', 223),
+(12, 9, 'GeForce GTX', '1060', '6GB', 'GDDR5', '1582 MHz', '1797 MHz', 223),
+(13, 9, 'GeForce GTX', '1070', '8GB', 'GDDR5', '1556 MHz', '1771 MHz', 280),
+(14, 9, 'GeForce GTX', '1070Ti', '8GB', 'GDDR5', '1632 MHz', '1721 MHz', 280),
+(15, 9, 'GeForce GTX', '1080', '8GB', 'GDDR5', '1657 MHz', '1797 MHz', 280),
+(16, 9, 'GeForce GTX', '1080Ti', '11GB', 'GDDR5X', '1632 MHz', '1746 MHz', 267),
+(17, 2, 'GeForce GTX', '1650', '4GB', 'GDDR6', '1410 MHz', '1680 MHz', 204),
+(18, 2, 'GeForce GTX', '1650S', '4GB', 'GDDR6', '1530 MHz', '1815 MHz', 243),
+(19, 9, 'GeForce GTX', '1650', '4GB', 'GDDR6', '1410 MHz', '1590 MHz', 191),
+(20, 9, 'GeForce GTX', '1650S', '4GB', 'GDDR6', '1530 MHz', '1740 MHz', 172),
+(21, 2, 'GeForce GTX', '1660', '6GB', 'GDDR6', '1530 MHz', '1845 MHz', 205),
+(22, 2, 'GeForce GTX', '1660S', '6GB', 'GDDR6', '1530 MHz', '1875 MHz', 243),
+(23, 2, 'GeForce GTX', '1660Ti', '6GB', 'GDDR6', '1500 MHz', '1890 MHz', 301),
+(24, 9, 'GeForce GTX', '1660', '6GB', 'GDDR6', '1530 MHz', '1860 MHz', 280),
+(25, 9, 'GeForce GTX', '1660S', '6GB', 'GDDR6', '1530 MHz', '1860 MHz', 280),
+(26, 9, 'GeForce GTX', '1660Ti', '6GB', 'GDDR6', '1500 MHz', '1860 MHz', 280),
+(27, 12, 'GeForce GTX', '1060', '6GB', 'GDDR5', '1506 MHz', '1709 MHz', 250),
+(28, 12, 'GeForce GTX', '1070', '8GB', 'GDDR5', '1506 MHz', '1683 MHz', 267),
+(29, 12, 'GeForce GTX', '1070Ti', '8GB', 'GDDR5', '1607 MHz', '1683 MHz', 267),
+(30, 12, 'GeForce GTX', '1080', '8GB', 'GDDR5', '1607 MHz', '1733 MHz', 267),
+(31, 12, 'GeForce GTX', '1080Ti', '11GB', 'GDDR5X', '1480 MHz', '1582 MHz', 267),
+(32, 2, 'GeForce RTX', '2060', '6GB', 'GDDR6', '1365 MHz', '1860 MHz', 300),
+(33, 2, 'GeForce RTX', '2060S', '8GB', 'GDDR6', '1500 MHz', '1860 MHz', 301),
+(34, 2, 'GeForce RTX', '2070', '8GB', 'GDDR6', '1410 MHz', '1845 MHz', 305),
+(35, 2, 'GeForce RTX', '2070S', '8GB', 'GDDR6', '1635 MHz', '1935 MHz', 300),
+(36, 2, 'GeForce RTX', '2080', '8GB', 'GDDR6', '1515 MHz', '1890 MHz', 300),
+(37, 2, 'GeForce RTX', '2080S', '8GB', 'GDDR6', '1650 MHz', '1890 MHz', 300),
+(38, 2, 'GeForce RTX', '2080Ti', '11GB', 'GDDR6', '1350 MHz', '1815 MHz', 310),
+(39, 9, 'GeForce RTX', '2060', '6GB', 'GDDR6', '1680 MHz', '1770 MHz', 265),
+(40, 9, 'GeForce RTX', '2060S', '8GB', 'GDDR6', '1470 MHz', '1680 MHz', 265),
+(41, 9, 'GeForce RTX', '2070', '8GB', 'GDDR6', '1620 MHz', '1740 MHz', 280),
+(42, 9, 'GeForce RTX', '2070S', '8GB', 'GDDR6', '1605 MHz', '1815 MHz', 286),
+(43, 9, 'GeForce RTX', '2080', '8GB', 'GDDR6', '1710 MHz', '1890 MHz', 290),
+(44, 9, 'GeForce RTX', '2080S', '8GB', 'GDDR6', '1650 MHz', '1845 MHz', 286),
+(45, 9, 'GeForce RTX', '2080Ti', '11GB', 'GDDR6', '1350 MHz', '1770 MHz', 290),
+(46, 2, 'GeForce RTX', '3060', '12GB', 'GDDR6', '1320 MHz', '1807 MHz', 177),
+(47, 2, 'GeForce RTX', '3060Ti', '8GB', 'GDDR6', '1410 MHz', '1890 MHz', 318),
+(48, 2, 'GeForce RTX', '3070', '8GB', 'GDDR6', '1500 MHz', '1905 MHz', 318),
+(49, 2, 'GeForce RTX', '3070Ti', '8GB', 'GDDR6X', '1575 MHz', '1815 MHz', 300),
+(50, 2, 'GeForce RTX', '3080', '10GB', 'GDDR6X', '1440 MHz', '1935 MHz', 318),
+(51, 2, 'GeForce RTX', '3080Ti', '12GB', 'GDDR6X', '1365 MHz', '1860 MHz', 293),
+(52, 2, 'GeForce RTX', '3090', '24GB', 'GDDR6X', '1395 MHz', '1890 MHz', 318),
+(53, 9, 'GeForce RTX', '3060', '12GB', 'GDDR6', '1320 MHz', '1867 MHz', 296),
+(54, 9, 'GeForce RTX', '3060Ti', '8GB', 'GDDR6', '1410 MHz', '1785 MHz', 296),
+(55, 9, 'GeForce RTX', '3070', '8GB', 'GDDR6', '1500 MHz', '1845 MHz', 290),
+(56, 9, 'GeForce RTX', '3070Ti', '8GB', 'GDDR6X', '1575 MHz', '1875 MHz', 324),
+(57, 9, 'GeForce RTX', '3080', '10GB', 'GDDR6X', '1710 MHz', '1905 MHz', 319),
+(58, 9, 'GeForce RTX', '3080Ti', '12GB', 'GDDR6X', '1365 MHz', '1770 MHz', 320),
+(59, 9, 'GeForce RTX', '3090', '24GB', 'GDDR6X', '1395 MHz', '1860 MHz', 319),
+(60, 2, 'Radeon RX', '5500 XT', '8GB', 'GDDR6', '1607 MHz', '1865 MHz', 280),
+(61, 2, 'Radeon RX', '5600 XT', '6GB', 'GDDR6', '1247 MHz', '1770 MHz', 303),
+(62, 2, 'Radeon RX', '5700', '8GB', 'GDDR6', '1610 MHz', '1750 MHz', 305),
+(63, 2, 'Radeon RX', '5700 XT', '8GB', 'GDDR6', '1770 MHz', '2035 MHz', 305),
+(64, 2, 'Radeon RX', '6600', '8GB', 'GDDR6', '1626 MHz', '2491 MHz', 243),
+(65, 2, 'Radeon RX', '6600 XT', '8GB', 'GDDR6', '1968 MHz', '2607 MHz', 243),
+(66, 2, 'Radeon RX', '6700 XT', '12GB', 'GDDR6', '2321 MHz', '2581 MHz', 322),
+(67, 2, 'Radeon RX', '6800', '16GB', 'GDDR6', '1700 MHz', '2190 MHz', 320),
+(68, 2, 'Radeon RX', '6800 XT', '16GB', 'GDDR6', '1825 MHz', '2340 MHz', 310),
+(69, 2, 'Radeon RX', '6900 XT', '16GB', 'GDDR6', '1825 MHz', '2340 MHz', 320),
+(70, 9, 'Radeon RX', '5500 XT', '8GB', 'GDDR6', '1685 MHz', '1845 MHz', 281),
+(71, 9, 'Radeon RX', '5600 XT', '6GB', 'GDDR6', '1247 MHz', '1620 MHz', 228),
+(72, 9, 'Radeon RX', '5700', '8GB', 'GDDR6', '1565 MHz', '1750 MHz', 280),
+(73, 9, 'Radeon RX', '5700 XT', '8GB', 'GDDR6', '1650 MHz', '1905 MHz', 280),
+(74, 9, 'Radeon RX', '6600', '8GB', 'GDDR6', '1626 MHz', '2491 MHz', 282),
+(75, 9, 'Radeon RX', '6600 XT', '8GB', 'GDDR6', '1968 MHz', '2607 MHz', 282),
+(76, 9, 'Radeon RX', '6700 XT', '12GB', 'GDDR6', '2321 MHz', '2581 MHz', 281),
+(77, 9, 'Radeon RX', '6800', '16GB', 'GDDR6', '1815 MHz', '2155 MHz', 286),
+(78, 9, 'Radeon RX', '6800 XT', '16GB', 'GDDR6', '1825 MHz', '2310 MHz', 324),
+(79, 9, 'Radeon RX', '6900 XT', '16GB', 'GDDR6', '2015 MHz', '2365 MHz', 322);
 
 -- --------------------------------------------------------
 
@@ -404,6 +476,21 @@ INSERT INTO `socket` (`Socket_id`, `Socket`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Storage`
+--
+
+CREATE TABLE `Storage` (
+  `storage_id` int(11) NOT NULL,
+  `Manufacturer_id` int(11) NOT NULL,
+  `Name` text NOT NULL,
+  `Capacity` text NOT NULL,
+  `Cache` text NOT NULL,
+  `Read/write speed` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `storage type`
 --
 
@@ -431,7 +518,6 @@ INSERT INTO `storage type` (`storage_id`, `storage type`) VALUES
 ALTER TABLE `case_pc`
   ADD PRIMARY KEY (`case_id`),
   ADD KEY `Manufacturer_id` (`Manufacturer_id`),
-  ADD KEY `length_id` (`length_id`),
   ADD KEY `ff_id` (`ff_id`);
 
 --
@@ -466,14 +552,7 @@ ALTER TABLE `form factor`
 --
 ALTER TABLE `gpu`
   ADD PRIMARY KEY (`gpu_id`),
-  ADD KEY `gpu_Manufacturer` (`Manufacturer_id`),
-  ADD KEY `length` (`length_id`);
-
---
--- Indexes for table `gpu length`
---
-ALTER TABLE `gpu length`
-  ADD PRIMARY KEY (`length_id`);
+  ADD KEY `gpu_Manufacturer` (`Manufacturer_id`);
 
 --
 -- Indexes for table `manufacturer`
@@ -519,6 +598,13 @@ ALTER TABLE `socket`
   ADD PRIMARY KEY (`Socket_id`);
 
 --
+-- Indexes for table `Storage`
+--
+ALTER TABLE `Storage`
+  ADD PRIMARY KEY (`storage_id`),
+  ADD KEY `Manufacturer_id` (`Manufacturer_id`);
+
+--
 -- Indexes for table `storage type`
 --
 ALTER TABLE `storage type`
@@ -562,13 +648,7 @@ ALTER TABLE `form factor`
 -- AUTO_INCREMENT for table `gpu`
 --
 ALTER TABLE `gpu`
-  MODIFY `gpu_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `gpu length`
---
-ALTER TABLE `gpu length`
-  MODIFY `length_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `gpu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT for table `manufacturer`
@@ -607,6 +687,12 @@ ALTER TABLE `socket`
   MODIFY `Socket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `Storage`
+--
+ALTER TABLE `Storage`
+  MODIFY `storage_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `storage type`
 --
 ALTER TABLE `storage type`
@@ -638,8 +724,7 @@ ALTER TABLE `cpu_cooler`
 -- Constraints for table `gpu`
 --
 ALTER TABLE `gpu`
-  ADD CONSTRAINT `gpu_Manufacturer` FOREIGN KEY (`Manufacturer_id`) REFERENCES `manufacturer` (`Manufacturer_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `gpu_length` FOREIGN KEY (`length_id`) REFERENCES `gpu length` (`length_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `gpu_Manufacturer` FOREIGN KEY (`Manufacturer_id`) REFERENCES `manufacturer` (`Manufacturer_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Constraints for table `motherboard`
@@ -663,6 +748,13 @@ ALTER TABLE `power supply`
 ALTER TABLE `ram`
   ADD CONSTRAINT `ram1` FOREIGN KEY (`Manufacturer_id`) REFERENCES `manufacturer` (`Manufacturer_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `ram2` FOREIGN KEY (`ram_type_id`) REFERENCES `ram type` (`ram_type_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Storage`
+--
+ALTER TABLE `Storage`
+  ADD CONSTRAINT `manufact` FOREIGN KEY (`Manufacturer_id`) REFERENCES `manufacturer` (`Manufacturer_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `storage1` FOREIGN KEY (`storage_id`) REFERENCES `storage type` (`storage_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
