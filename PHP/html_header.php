94a6159ba2396b1c89de9a5ad8357459981c8e48
Deniel0 <92303546+Deniel0@users.noreply.github.com>
2022-02-20 21:33:54 +0100

    somethings

--- PHP/html_header.php
+++ PHP/html_header.php
@@ -3,6 +3,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Partpicker</title>
+  <link rel="shortcut icon" href="icon.png" type="image/x-icon">
   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/56bce479be.js" crossorigin="anonymous"></script>
@@ -15,8 +16,9 @@
     <div id="mainListDiv" class="main_list">
         <ul class="navlinks">
             <li><a href="index.php"><i class="fa-solid fa-house"></i> Home</a></li>
-            <li><a href="Picker.php"><i class="fa-solid fa-desktop"></i> Picker</a></li>
-            <li><a href="Components.php"><i class="fa-solid fa-puzzle-piece"></i> Components</a></li>
+            <li><a href="Picker.php"><i class="fa-solid fa-puzzle-piece"></i> Picker</a></li>
+            <li><a href="complete_builds.php"><i class="fa-solid fa-desktop"></i> Complete Builds</a></li>
+            <li><a href="Components.php"><i class="fa-solid fa-list"></i> Components</a></li>
             <li><a href="login.php"><i class="fa-solid fa-right-to-bracket"></i> Log in</a></li>
             <li><a href="signup.php"><i class="fa-solid fa-user-plus"></i> Sign up</a></li>
             <li><a href="includes/logout.php"><i class="fa-solid fa-power-off"></i> Log out</a></li>
