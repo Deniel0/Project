6846e6acd70d0a48db98189bd454a32e96209cda
Deniel0 <92303546+Deniel0@users.noreply.github.com>
2022-02-21 15:37:30 +0100

    Responsive navbar
    95% done

--- PHP/html_header.php
+++ PHP/html_header.php
@@ -2,18 +2,17 @@
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Title -->
   <title>Partpicker</title>
+  <!-- Title icon -->
   <link rel="shortcut icon" href="icon.png" type="image/x-icon">
-  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> -->
+  <!-- css links -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <script src="https://kit.fontawesome.com/56bce479be.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="header.css">
 </head>
-<!-- navbar -->
 <body>
+<!-- navbar -->
 <nav class="nav">
-  <div class="container" id="navfont-size">
-    <div id="mainListDiv" class="main_list">
     <div class="hamburger">
       <div class="line1"></div>
       <div class="line2"></div>
@@ -26,16 +25,14 @@
             <li><a href="Components.php"><i class="fa-solid fa-list"></i> Components</a></li>
             <li><a href="login.php"><i class="fa-solid fa-right-to-bracket"></i> Log in</a></li>
             <li><a href="signup.php"><i class="fa-solid fa-user-plus"></i> Sign up</a></li>
-            <li><a href="includes/logout.php"><i class="fa-solid fa-power-off"></i> Log out</a></li>
+            <li class="blogout"><a href="includes/logout.php"><i class="fa-solid fa-power-off"></i> Log out</a></li>
         </ul>
-    </div>
-  </div>
 </nav>
-    <section class="home">
-    </section>
-    <br>
-
+<!-- Home image -->
+<section class="home"></section><br>
 <!-- script -->
+<script src="header.js"></script>
+<script src="https://kit.fontawesome.com/56bce479be.js" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
