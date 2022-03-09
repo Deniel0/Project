ae0848ca4ad6e5af55c889dbe5a426c36738c903
Deniel0 <92303546+Deniel0@users.noreply.github.com>
2022-03-09 10:37:08 +0100

    home page nearly done

--- PHP/index.php
+++ PHP/index.php
@@ -7,10 +7,9 @@
 </head>
 <body>
     <div class="container">
-        <h1 class="main-header">Build your own gaming PC</h1>
+        <div class="title-style">
+            <h1 class="main-header pulsate"><span class="bigger">Build your</span> <br> own gaming PC</h1>
+        </div>
         <button class="startb" role="button"><span class="text"><a href="Picker.php">Let's get started</a></span></button>
     </div>
 </body>
-<?php
-    // include_once 'footer.php';
-?>
