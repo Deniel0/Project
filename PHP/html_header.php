c7ad467ee86305895d2fdb9f28d79b2325e190c8
Deniel0 <92303546+Deniel0@users.noreply.github.com>
2022-03-09 08:19:21 +0100

    home
    button design done

--- PHP/html_header.php
+++ PHP/html_header.php
@@ -11,7 +11,7 @@ session_start();
   <link rel="shortcut icon" href="images/icon.png" type="image/x-icon">
   <!-- css links -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/header.css">
+  <link rel="stylesheet" href="css/header2.css">
 </head>
 <body>
 <!-- navbar -->
--- PHP/index.php
+++ PHP/index.php
@@ -6,8 +6,11 @@
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-
+    <div class="container">
+        <h1 class="main-header">Build your own gaming PC</h1>
+        <button class="startb" role="button"><span class="text"><a href="Picker.php">Let's get started</a></span></button>
+    </div>
 </body>
 <?php
-    include_once 'footer.php';
+    // include_once 'footer.php';
 ?>
