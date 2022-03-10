9ca65d9ec655bc2e28ba995580948a25f62819a7
Deniel0 <sutod820@gmail.com>
2022-03-10 11:58:32 +0100

    completeb kiiras

--- PHP/complete_builds.php
+++ PHP/complete_builds.php
@@ -8,18 +8,162 @@
 <body>
 <div class="content">
     <ul class="flex-container wrap">
-        <a href="first.pc.php"><div class="card"><li class="flex-item"><img src="images/gamingpc.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div></a>
-        <div class="card"><li class="flex-item"><img src="images/spirit.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/gamingpc.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/spirit.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/gamingpc.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/spirit.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/gamingpc.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/spirit.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/gamingpc.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/spirit.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/gamingpc.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
-        <div class="card"><li class="flex-item"><img src="images/spirit.png" alt="gamingpc"><p>Intel Core i7-12700k with RTX 3090</p></li></div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/gamingpc.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=1";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/spirit.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=2";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/gamingpc.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=3";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/spirit.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=4";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/gamingpc.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=5";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/spirit.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=6";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/gamingpc.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=7";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/spirit.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=8";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/gamingpc.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=9";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/spirit.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=10";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/gamingpc.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=11";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
+        <div class="card">
+            <li class="flex-item">
+                <img src="images/spirit.png" alt="gamingpc">
+                <?php
+                    $sql = "SELECT cpu, motherboard, cpu_cooler, ram, gpu, first_storage, sec_storage, pc_case, power_supply FROM complete_builds WHERE cb_id=12";
+                    $result = $conn->query($sql);
+                    $resultCheck = $result->num_rows;
+                    while ($row = mysqli_fetch_array($result)){
+                        echo "Cpu: ".$row['cpu']."<br>"."Motherboard: ".$row['motherboard']."<br>"."Cpu cooler: ".$row['cpu_cooler']."<br>"."Ram: ".$row['ram']."<br>"."Video Card: ".$row['gpu']."<br>"."1st Storage: ".$row['first_storage']."<br>"."2nd Storage: ".$row['sec_storage']."<br>"."Case: ".$row['pc_case']."Power Supply: ".$row['power_supply'];
+                    }
+                ?>
+            </li>
+        </div>
     </ul>
 </div>
 </body>
