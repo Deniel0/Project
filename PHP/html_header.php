e34907c204c4b60612580aa72078391477936571
Deniel0 <sutod820@gmail.com>
2022-03-17 12:27:45 +0100

    logo

--- PHP/html_header.php
+++ PHP/html_header.php
@@ -8,7 +8,7 @@ session_start();
   <!-- Title -->
   <title>Partpicker</title>
   <!-- Title icon -->
-  <link rel="shortcut icon" href="images/icon.png" type="image/x-icon">
+  <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
   <!-- css links -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="css/header2.css">
