30b525fed6efee03bb43a256b27bbba105c471e6
Deniel0 <sutod820@gmail.com>
2022-03-21 12:00:00 +0100

    Revert "Update html_header.php"
    This reverts commit e7d9312fc2809cb88b4611ca73e455f371266293.

--- PHP/html_header.php
+++ PHP/html_header.php
@@ -1,6 +1,5 @@
-<?php
-    session_name('partpicker');
-    session_start();
+<?php 
+session_start();
 ?>
 <head>
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
