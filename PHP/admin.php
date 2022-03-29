c591b3e68325a1a7bdf618e9d60dbb8563b461c4
Deniel0 <sutod820@gmail.com>
2022-03-29 12:16:48 +0200

    admin

--- PHP/admin.php
+++ PHP/admin.php
@@ -19,43 +19,40 @@
             <li class="flex-item">
             <form action="" method="POST">
             <h4>Admin:</h4>
-                <input required type="number" pattern="[0-9]{3}" min="1" name="admin" id="">
+                <input required type="number" pattern="[0-9]" min="1" name="admin" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Admin</button>
             </form>
             <form action="" method="POST">
                 <h4>Manufacturer:</h4>
-                <input required type="text" name="man_name" id="">
+                <input required pattern="[A-Za-z]" type="text" name="man_name" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Manufacturer</button>
             </form>
             <form action="" method="POST">
                 <h4>Chipset:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="chip_name" id="">
+                <input required pattern="[A-Za-z]" type="text" name="chip_name" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Chipset</button>
             </form>
-            <?php
-                $sql = "INSERT INTO `chipset`(`chipset_id`, `chipset`) VALUES ('[value-2]')";
-            ?>
             <form action="" method="POST">
                 <h4>Ram type:</h4>
-                <input required pattern="[A-Za-z]{4}" type="text" name="ram_type" id="">
+                <input required pattern="[A-Za-z]" type="text" name="ram_type" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Ram type</button>
             </form>
-            <?php
-                /*$sql = "INSERT INTO `ram_type`(`ram_type_id`, `model`) VALUES ('[value-2]')";*/
-            ?>
             <form action="" method="POST">
                 <h4>Storage type:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="stype_name" id="">
+                <input required pattern="[A-Za-z]" type="text" name="stype_name" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Storage type</button>
             </form>
-            <?php
-                /*$sql = "INSERT INTO `storage_type`(`storage_type_id`, `s_type`) VALUES ('[value-2]')";*/
-            ?>
+            <form action="" method="POST">
+                <h4>Socket:</h4>
+                <input required pattern="[A-Za-z]" type="text" name="stype_name" id="">
+                <br>
+                <button class="button-6" role="button" type="submit">Add Storage type</button>
+            </form>
             </li>
         </div>
         <!-- CPU -->
@@ -63,8 +60,18 @@
             <li class="flex-item">
             <h3>Add CPU:</h3>
             <form action="" method="POST">
-                <h4>Socket id:</h4>
-                <input required type="number" pattern="[0-9]{3}" min="1" name="socket" id="">
+                <h4>Socket:</h4>
+                <select required name="Socket">
+                    <option value="" selected disabled>Select Socket...</option>
+                    <?php
+                        $sql = "SELECT * FROM socket";
+                        $result = mysqli_query($conn, $sql);
+                        $resultCheck = mysqli_num_rows($result);
+                        while ($row = mysqli_fetch_array($result)){
+                            echo "<option value='".$row['Socket']."'>".$row['Socket']."</option>" ;
+                        }
+                    ?>
+                    </select><br>
                 <h4>Manufacturer:</h4>
                     <select required name="Manufacturer">
                     <option value="" selected disabled>Select Manufacturer...</option>
@@ -78,26 +85,22 @@
                     ?>
                     </select><br>
                 <h4>Cpu name:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="c_name" id="">
+                <input required pattern="[A-Za-z]" type="text" name="c_name" id="">
                 <h4>Number of seeds:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="seeds" id="">
+                <input required pattern="[A-Za-z]" type="text" name="seeds" id="">
                 <h4>Number of threads:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="threads" id="">
+                <input required pattern="[A-Za-z]" type="text" name="threads" id="">
                 <h4>Clock:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="clock" id="">
+                <input required pattern="[A-Za-z]" type="text" name="clock" id="">
                 <h4>Turbo clock:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="Turbo clock" id="">
+                <input required pattern="[A-Za-z]" type="text" name="Turbo clock" id="">
                 <h4>Size of L3 cache:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="l3-cache" id="">
+                <input required pattern="[A-Za-z]" type="text" name="l3-cache" id="">
                 <h4>Wattage:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="TDP" id="">
+                <input required pattern="[A-Za-z]" type="text" name="TDP" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add CPU</button>
             </form>
-            <?php
-                /*$sql = "$sql = "INSERT INTO `cpu`(`cpu_id`, `Socket_id`, `Manufacturer_id`, `Name`, `Seeds`, `Threads`, `Clock`, `Turbo clock`, `L3 cache`, `TDP`)
-                        VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]','[value-10]')"";*/
-            ?>
             </li>
         </div>
         <!-- Motherboard -->
@@ -105,8 +108,18 @@
             <li class="flex-item">
             <h3>Add Motherboard:</h3>
             <form action="yo.php" method="POST">
-                <h4>Socket id:</h4>
-                <input required type="number" pattern="[0-9]{3}" min="1" name="socket" id="">
+            <h4>Socket:</h4>
+                <select required name="Socket">
+                    <option value="" selected disabled>Select Socket...</option>
+                    <?php
+                        $sql = "SELECT * FROM socket";
+                        $result = mysqli_query($conn, $sql);
+                        $resultCheck = mysqli_num_rows($result);
+                        while ($row = mysqli_fetch_array($result)){
+                            echo "<option value='".$row['Socket']."'>".$row['Socket']."</option>" ;
+                        }
+                    ?>
+                    </select><br>
                 <h4>Manufacturer id:</h4>
                 <select required name="Manufacturer">
                 <option value="" selected disabled>Select Manufacturer...</option>
@@ -119,19 +132,25 @@
                     }
                 ?>
                 </select><br>
-                <h4>Chipset id:</h4>
-                <input required type="number" pattern="[0-9]{3}" min="1" name="chipset" id="">
+                <h4>Chipset:</h4>
+                <select required name="Chipset">
+                    <option value="" selected disabled>Select Chipset...</option>
+                    <?php
+                        $sql = "SELECT * FROM chipset";
+                        $result = mysqli_query($conn, $sql);
+                        $resultCheck = mysqli_num_rows($result);
+                        while ($row = mysqli_fetch_array($result)){
+                            echo "<option value='".$row['chipset']."'>".$row['chipset']."</option>" ;
+                        }
+                    ?>
+                    </select><br>
                 <h4>M.2 compatibility (0/1):</h4>
-                <input required type="number" pattern="[0-9]{3}" min="0" max="1" name="m2_comp" id="">
+                <input required type="number" pattern="[0-9]" min="0" max="1" name="m2_comp" id="">
                 <h4>Motherboard name:</h4>
                 <input required pattern="[A-Za-z]{16}" type="text" name="m_name" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Motherboard</button>
             </form>
-            <?php
-                /*$sql = "$sql = "INSERT INTO `motherboard`(`motherboard_id`, `Socket_id`, `Manufacturer_id`, `chipset_id`, `m2_comp`, `Name`)
-                        VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]')"";*/
-            ?>
             </li>
         </div>
         <!-- CPU cooler -->
@@ -152,11 +171,11 @@
                 ?>
                 </select><br>
                 <h4>Model:</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="model" id="">
+                <input required pattern="[A-Za-z]" type="text" name="model" id="">
                 <h4>Type</h4>
-                <input required pattern="[A-Za-z]{12}" type="text" name="type" id="">
+                <input required pattern="[A-Za-z]" type="text" name="type" id="">
                 <h4>RPM:</h4>
-                <input required type="number" pattern="[0-9]{3}" min="1" name="rmp" id="">
+                <input required type="number" pattern="[0-9]" min="1" name="rmp" id="">
                 <h4>Height:</h4>
                 <input required pattern="[A-Za-z]{8}" type="text" name="height" id="">
                 <h4>Fan size (in mm):</h4>
@@ -164,10 +183,6 @@
                 <br>
                 <button class="button-6" role="button" type="submit">Add CPU cooler</button>
             </form>
-            <?php
-                /*$sql = "$sql = "INSERT INTO `cpu_cooler`(`cooler_id`, `Manufacturer_id`, `Model`, `Type`, `RPM`, `Height`, `Fan size`)
-                        VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')"";*/
-            ?>
             </li>
         </div>
         <!-- RAM -->
@@ -176,7 +191,7 @@
             <h3>Add RAM:</h3>
             <form action="yo.php" method="POST">
                 <h4>Socket id:</h4>
-                <input required type="number" pattern="[0-9]{3}" min="1" name="socket" id="">
+                <input required type="number" pattern="[0-9]" min="1" name="socket" id="">
                 <h4>Manufacturer id:</h4>
                 <select required name="Manufacturer">
                 <option value="" selected disabled>Select Manufacturer...</option>
@@ -206,16 +221,12 @@
                 <h4>Speed (Mhz):</h4>
                 <input required pattern="[A-Za-z]{7}" type="text" name="speed" id="">
                 <h4>Modules:</h4>
-                <input required type="number" pattern="[0-9]{3}" min="1" max="8" name="modules" id="">
+                <input required type="number" pattern="[0-9]" min="1" max="8" name="modules" id="">
                 <h4>Cas Latency:</h4>
-                <input required pattern="[A-Za-z]{4}" type="text" name="cas_Latency" id="">
+                <input required pattern="[A-Za-z]" type="text" name="cas_Latency" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add RAM</button>
             </form>
-            <?php
-                /*$sql = "$sql = "INSERT INTO `ram`(`ram_id`, `Manufacturer_id`, `ram_type_id`, `Capacity`, `Speed`, `Modules`, `CAS_Latency`)
-                        VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')"";*/
-            ?>
             </li>
         </div>
         <!-- GPU -->
@@ -236,26 +247,22 @@
                 ?>
                 </select><br>
                 <h4>Type:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="type" id="">
+                <input required pattern="[A-Za-z]" type="text" name="type" id="">
                 <h4>GPU name:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="g_name" id="">
+                <input required pattern="[A-Za-z]" type="text" name="g_name" id="">
                 <h4>Memory size(GB):</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="memory_size" id="">
+                <input required pattern="[A-Za-z]" type="text" name="memory_size" id="">
                 <h4>Memory type:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="memory_type" id="">
+                <input required pattern="[A-Za-z]" type="text" name="memory_type" id="">
                 <h4>Core clock(Mhz):</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="core_clock" id="">
+                <input required pattern="[A-Za-z]" type="text" name="core_clock" id="">
                 <h4>Boost clock(Mhz):</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="boost_clock" id="">
+                <input required pattern="[A-Za-z]" type="text" name="boost_clock" id="">
                 <h4>Length(mm):</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="length" id="">
+                <input required pattern="[A-Za-z]" type="text" name="length" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add GPU</button>
             </form>
-            <?php
-                /*$sql = "$sql = "INSERT INTO `gpu`(`gpu_id`, `Manufacturer_id`, `Type`, `Name`, `Memory_size`, `Memory_type`, `Core_clock`, `Boost_clock`, `Length`)
-                        VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]')"";*/
-            ?>
             </li>
         </div>
         <!-- Storage -->
@@ -288,20 +295,16 @@
                     ?>
                     </select><br>
                 <h4>Storage name:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="s_name" id="">
+                <input required pattern="[A-Za-z]" type="text" name="s_name" id="">
                 <h4>Capacity:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="capacity" id="">
+                <input required pattern="[A-Za-z]" type="text" name="capacity" id="">
                 <h4>Read/Write speed:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="r_w_speed" id="">
+                <input required pattern="[A-Za-z]" type="text" name="r_w_speed" id="">
                 <h4>M.2 compatibility (0/1):</h4>
-                <input required type="number" pattern="[0-9]{3}" min="0" max="1" name="m2_comp" id="">
+                <input required type="number" pattern="[0-9]" min="0" max="1" name="m2_comp" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Storage</button>
             </form>
-            <?php
-                /*$sql = "$sql = "INSERT INTO `storage`(`storage_id`, `Manufacturer_id`, `storage_type_id`, `Name`, `Capacity`, `R_W_speed`, `m2_comp`)
-                        VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]')"";*/
-            ?>
             </li>
         </div>
         <!-- Case -->
@@ -322,16 +325,12 @@
                 ?>
                 </select><br>
                 <h4>Case name:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="case_name" id="">
+                <input required pattern="[A-Za-z]" type="text" name="case_name" id="">
                 <h4>Max length(mm):</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="max_length" id="">
+                <input required pattern="[A-Za-z]" type="text" name="max_length" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Case</button>
             </form>
-            <?php
-                /*$sql = "$sql = "INSERT INTO `case_pc`(`case_id`, `Manufacturer_id`, `Name`, `Max_Length`)
-                        VALUES ('[value-2]','[value-3]','[value-4]')"";*/
-            ?>
             </li>
         </div>
         <!-- Power Supply -->
@@ -352,21 +351,16 @@
                 ?>
                 </select><br>
                 <h4>Power Supply name:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="pw_name" id="">
+                <input required pattern="[A-Za-z]" type="text" name="pw_name" id="">
                 <h4>Wattage(W):</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="wattage" id="">
+                <input required pattern="[A-Za-z]" type="text" name="wattage" id="">
                 <h4>Efficiency_Rating:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="efficiency_rating" id="">
+                <input required pattern="[A-Za-z]" type="text" name="efficiency_rating" id="">
                 <h4>Modular:</h4>
-                <input required pattern="[A-Za-z]{3}" type="text" name="modular" id="">
+                <input required pattern="[A-Za-z]" type="text" name="modular" id="">
                 <br>
                 <button class="button-6" role="button" type="submit">Add Power Supply</button>
             </form>
-            <?php
-                /*$sql = "$sql = "INSERT INTO `power_supply`(`pw_id`, `Manufacturer_id`, `Name`, `Wattage`, `Efficiency_Rating`, `Modular`)
-                        VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]')"";*/
-            /* */
-            ?>
             </li>
         </div>
         </ul>
--- PHP/adminadd.php
+++ PHP/adminadd.php
@@ -1,48 +1,49 @@
 <?php
     require 'includes/db.includes.php';
     if(isset($_POST['man_name'])){
-        $sql = "INSERT INTO manufacturer (Manufacturer) VALUES (".$_POST["man_name"] .")";
+        $sql = "INSERT INTO manufacturer (Manufacturer) VALUES ('".$_POST["man_name"] ."')";
     }
     if(isset($_POST['admin'])){
-        $sql = "INSERT INTO `admin`(`user_id`,`prev`) Select user_id,1 from users where username='".$_POST["admin"]."'";
+        $sql = "INSERT INTO `admin`(`user_id`,`prev`) SELECT user_id,1 FROM users WHERE username='".$_POST["admin"]."'";
     }
     if(isset($_POST['chip_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["chip_name"] .")";
+        $sql = "INSERT INTO `chipset`(`chipset`) VALUES ('".$_POST["chip_name"] ."')";
     }
     if(isset($_POST['ram_type'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["ram_type"] .")";
+        $sql = "INSERT INTO `ram_type`(`model`) VALUES ('".$_POST["ram_type"] ."')";
     }
     if(isset($_POST['stype_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["stype_name"] .")";
+        $sql = "INSERT `storage_type`(`s_type`) INTO  VALUES ('".$_POST["stype_name"] ."')";
     }
     if(isset($_POST['socket'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["socket"] .")";
+        $sql = "INSERT `socket`(`Socket`) INTO  VALUES ('".$_POST["socket"] ."')";
     }
     if(isset($_POST['c_name'],$_POST['seeds'],$_POST['threads'],$_POST['clock'],$_POST['Turbo clock'],$_POST['l3-cache'],$_POST['TDP'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST['c_name']."".$_POST['seeds']."".$_POST['threads']."".$_POST['clock']."".$_POST['Turbo clock']."".$_POST['l3-cache']."".$_POST['TDP'].")";
+        $sql = "INSERT INTO `cpu`(`Socket_id`, `Manufacturer_id`, `Name`, `Seeds`, `Threads`, `Clock`, `Turbo clock`, `L3 cache`, `TDP`) VALUES ('".$_POST['c_name']."''".$_POST['seeds']."'".$_POST['threads']."'".$_POST['clock']."'".$_POST['Turbo clock']."'".$_POST['l3-cache']."'".$_POST['TDP'].")";
     }
-    if(isset($_POST['man_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["man_name"] .")";
+    if(isset($_POST['Socket'],$_POST['Manufacturer'],$_POST['Chipset'],$_POST['m2_comp'],$_POST['m_name'])){
+        $sql = "INSERT INTO `motherboard`(`Socket_id`, `Manufacturer_id`, `chipset_id`, `m2_comp`, `Name`) VALUES ('".$_POST['Socket']."''".$_POST['Manufacturer']."'".$_POST['Chipset']."'".$_POST['m2_comp']."'".$_POST['m_name'].")";
     }
-    if(isset($_POST['man_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["man_name"] .")";
+    if(isset($_POST['Manufacturer'],$_POST['model'],$_POST['type'],$_POST['rmp'],$_POST['height'],$_POST['fan_size'])){
+        $sql = "INSERT INTO `cpu_cooler`(`Manufacturer_id`, `Model`, `Type`, `RPM`, `Height`, `Fan size`) VALUES ('".$_POST['Manufacturer']."''".$_POST['model']."'".$_POST['type']."'".$_POST['rmp']."'".$_POST['height']."'".$_POST['fan_size'].")";
     }
-    if(isset($_POST['man_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["man_name"] .")";
+    if(isset($_POST['socket'],$_POST['Manufacturer'],$_POST['ram_type'],$_POST['capacity'],$_POST['speed'],$_POST['modules'],$_POST['cas_Latency'])){
+        $sql = "INSERT INTO `ram`(`Manufacturer_id`, `ram_type_id`, `Capacity`, `Speed`, `Modules`, `CAS_Latency`) VALUES ('".$_POST['socket']."''".$_POST['Manufacturer']."'".$_POST['ram_type']."'".$_POST['capacity']."'".$_POST['speed']."'".$_POST['modules']."'".$_POST['cas_Latency'].")";
     }
-    if(isset($_POST['man_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["man_name"] .")";
+    if(isset($_POST['Manufacturer'],$_POST['type'],$_POST['g_name'],$_POST['memory_size'],$_POST['memory_type'],$_POST['core_clock'],$_POST['boost_clock'],$_POST['length'])){
+        $sql = "INSERT INTO `gpu`(`Manufacturer_id`, `Type`, `Name`, `Memory_size`, `Memory_type`, `Core_clock`, `Boost_clock`, `Length`) VALUES ('".$_POST['Manufacturer']."''".$_POST['type']."'".$_POST['g_name']."'".$_POST['memory_size']."'".$_POST['memory_type']."'".$_POST['core_clock']."'".$_POST['boost_clock']."'".$_POST['length'].")";
     }
-    if(isset($_POST['man_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["man_name"] .")";
+    if(isset($_POST['Manufacturer'],$_POST['storage_type'],$_POST['s_name'],$_POST['capacity'],$_POST['r_w_speed'],$_POST['m2_comp'])){
+        $sql = "INSERT INTO `storage`(`Manufacturer_id`, `storage_type_id`, `Name`, `Capacity`, `R_W_speed`, `m2_comp`) VALUES ('".$_POST['Manufacturer']."''".$_POST['model']."'".$_POST['type']."'".$_POST['rmp']."'".$_POST['height']."'".$_POST['fan_size'].")";
     }
-    if(isset($_POST['man_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["man_name"] .")";
+    if(isset($_POST['Manufacturer'],$_POST['case_name'],$_POST['max_length'])){
+        $sql = "INSERT INTO `case_pc`(`Manufacturer_id`, `Name`, `Max_Length`) VALUES ('".$_POST['Manufacturer']."''".$_POST['case_name']."'".$_POST['max_length'].")";
     }
-    if(isset($_POST['man_name'])){
-        $sql = "INSERT INTO manufacturer(Manufacturer) VALUES (".$_POST["man_name"] .")";
+    if(isset($_POST['Manufacturer'],$_POST['pw_name'],$_POST['wattage'],$_POST['efficiency_rating'],$_POST['modular'])){
+        $sql = "INSERT INTO `power_supply`(`Manufacturer_id`, `Name`, `Wattage`, `Efficiency_Rating`, `Modular`) VALUES ('".$_POST['Manufacturer']."''".$_POST['pw_name']."'".$_POST['wattage']."'".$_POST['efficiency_rating']."'".$_POST['modular'].")";
     }
     if (isset($sql)){
         $conn->query($sql);
+        header("location:admin.php");
     }
 ?>
