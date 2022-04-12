5c7f222757ae4b133bc9cbe41d1e76823c1400b2
Deniel0 <92303546+Deniel0@users.noreply.github.com>
2022-04-12 11:25:33 +0200

    F

--- Szakdoga/index.php
+++ Szakdoga/index.php
@@ -6,6 +6,20 @@
         <div class="title-style">
             <h1 class="main-header pulsate"><span class="bigger">Build your</span> <br> own gaming PC</h1>
         </div>
+    
+    <?php 
+        if(!isset($_SESSION['username'])){
+    ?>
+        <button class="startb"><span class="text"><a href="login2.php">Let's get started</a></span></button>
+    <?php 
+        }
+    ?>
+    <?php 
+        if(isset($_SESSION['username'])){
+    ?>
         <button class="startb"><span class="text"><a href="Picker.php">Let's get started</a></span></button>
+    <?php 
+        }
+    ?>
     </div>
 </body>
